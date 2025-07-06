981ea580ca5dcbe7e258a9a8a0669768c335efd5
Caio Vinicius <137227050+cvinicius369@users.noreply.github.com>
2025-07-06 14:58:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const toggleBtn = document.getElementById('theme-toggle');
+const icon = toggleBtn.querySelector('i');
+const banner = document.getElementById('banner');
+const body = document.body;
+
+toggleBtn.addEventListener('click', () => {
+  const isDark = banner.classList.contains('dark-theme');
+
+  if (isDark) {
+    banner.classList.remove('dark-theme');
+    banner.classList.add('light-theme');
+    body.classList.remove('dark');
+    body.classList.add('light');
+    icon.classList.remove('fa-moon');
+    icon.classList.add('fa-sun');
+  } else {
+    banner.classList.remove('light-theme');
+    banner.classList.add('dark-theme');
+    body.classList.remove('light');
+    body.classList.add('dark');
+    icon.classList.remove('fa-sun');
+    icon.classList.add('fa-moon');
+  }
+});
